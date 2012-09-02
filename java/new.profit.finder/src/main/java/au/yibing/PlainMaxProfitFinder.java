71272d993240520da6c37f2bfa2851207174e2b7
yibing <shi.yibing@gmail.com>
2012-09-02 22:15:45 +1000

    Partially OK. Use BufferedReader

--- java/new.profit.finder/src/main/java/au/yibing/PlainMaxProfitFinder.java
+++ java/new.profit.finder/src/main/java/au/yibing/PlainMaxProfitFinder.java
@@ -1,81 +1,70 @@
 package au.yibing;
 
-import org.apache.commons.cli.CommandLine;
-import org.apache.commons.cli.Options;
-import org.apache.commons.cli.ParseException;
-import org.apache.commons.cli.PosixParser;
-
 import java.io.*;
 import java.text.DecimalFormat;
 
 public class PlainMaxProfitFinder {
 
-    private static String inputFilePath;
-    private static String outputFilePath;
+    public static void main(String[] args) throws IOException{
+        String inputFilePath = "price_moves.csv";
+        String outputFilePath = "result.txt";
 
-    public static void main(String[] args) throws IOException, ParseException {
-        long start = System.nanoTime();
+        if (args.length > 0) {
+            inputFilePath = args[0];
+        }
 
-        parseArguments(args);
+        if (args.length > 1) {
+            outputFilePath = args[1];
+        }
 
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(new FileInputStream(inputFilePath)));
-        PriceInfo low = null;
-        PriceInfo high = null;
-        double maxProfit = 0;
-        PriceInfo sellPoint = null;
-        PriceInfo buyPoint = null;
+
+
+        double lowPointPrice = 1;
+        String lowPointDate = bufferedReader.readLine(); //set low point according to first line
+        double sellPointPrice = -1;
+        String sellPointDate = null;
+        double buyPointPrice = -1;
+        String buyPointDate = null;
         double currentPrice = 1;
+        double maxProfit = Double.MIN_VALUE;
         String line;
         while ((line = bufferedReader.readLine()) != null) {
             String[] fields = line.split(",");
-            double move;
-            String time;
-            if (fields.length == 1) {
-                move = 0;
-                time = fields[0];
-            } else {
-                move = Double.valueOf(fields[0]);
-                time = fields[1];
-            }
+            double move = Double.valueOf(fields[0]);
+            String date = fields[1];
             currentPrice = currentPrice * (move / 100 + 1);
-            PriceInfo priceInfo = new PriceInfo(currentPrice, time);
-            if (low == null || priceInfo.getPrice() < low.getPrice())
-                low = priceInfo;
-            if (high == null || priceInfo.getPrice() > high.getPrice())
-                high = priceInfo;
-            double curPossibleMaxProfit = priceInfo.getPrice() - low.getPrice();
+            double curPossibleMaxProfit = currentPrice - lowPointPrice;
             if (curPossibleMaxProfit > maxProfit) {
                 maxProfit = curPossibleMaxProfit;
-                sellPoint = priceInfo;
-                buyPoint = low;
+                sellPointPrice = currentPrice;
+                sellPointDate = date;
+                buyPointPrice = lowPointPrice;
+                buyPointDate = lowPointDate;
+            }
+            if (currentPrice < lowPointPrice) {
+                lowPointPrice = currentPrice;
+                lowPointDate = date;
             }
         }
-        if (buyPoint != null & sellPoint != null) {
-            writeResult(buyPoint, sellPoint, maxProfit);
+
+        if (buyPointDate != null & sellPointDate != null) {
+            writeResult(buyPointPrice, buyPointDate, sellPointPrice, sellPointDate, maxProfit, outputFilePath);
         }else {
             System.err.println("Cannot find any profit");
         }
-
-        System.out.println("Total time: " + (System.nanoTime() - start) / 1000000 + " milli seconds");
     }
 
-
-    private static void parseArguments(String[] args) throws ParseException {
-        Options options = new Options();
-        options.addOption("input", true, "The input file path");
-        options.addOption("output", true, "The output file path");
-
-        CommandLine commandLine = new PosixParser().parse(options, args);
-
-        inputFilePath = commandLine.getOptionValue("input", "price_moves.csv");
-        outputFilePath = commandLine.getOptionValue("output", "result.txt");
-    }
-
-    private static void writeResult(PriceInfo buyPoint, PriceInfo sellPoint, double profit) throws FileNotFoundException {
+    private static void writeResult(double buyPointPrice,
+                                    String buyPointDate,
+                                    double sellPointPrice,
+                                    String sellPointDate,
+                                    double profit,
+                                    String outputFilePath) throws FileNotFoundException {
         PrintWriter printWriter = new PrintWriter(new FileOutputStream(outputFilePath));
-        printWriter.println(buyPoint.getDateTime());
-        printWriter.println(sellPoint.getDateTime());
-        double profitRatio = profit / buyPoint.getPrice() * 100;
+        printWriter.println(buyPointDate);
+        printWriter.println(sellPointDate);
+        double profitRatio = profit / buyPointPrice * 100;
         DecimalFormat df = new DecimalFormat("#.###");
         printWriter.println(df.format(profitRatio));
         printWriter.flush();
